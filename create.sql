cf31fc5ec9e988d9d2a3ba1e7af57cc9e2610ce2
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-11-08 21:34:16 +0300

    Crete initial project structure and add sql queries to create database and tables in create.sql file

---
+++ create.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE education;
+use education;
+
+CREATE TABLE institution(
+    institutionid INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL,
+    PRIMARY KEY ( institutionid )
+    );
+
+CREATE TABLE course(
+    courseid INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL,
+    institution INT(11) NOT NULL,
+    PRIMARY KEY ( institutionid ),
+    FOREIGN KEY ( institution ) REFERENCES institution( institutionid )
+    );
+
+CREATE TABLE student(
+    studentid INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL,
+    course INT(11) NOT NULL,
+    PRIMARY KEY ( studentid ),
+    FOREIGN KEY ( course ) REFERENCES course( courseid )
+    );
